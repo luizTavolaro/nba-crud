9f7fb8b89209d9b0e184e39f5f6650c246aefd16
luizTavolaro <luiz.felipetaa@gmail.com>
2025-05-19 23:03:10 -0300

    feat: create table stat

---
+++ createTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE times (
+    id VARCHAR(36) PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    dataFundacao DATE NOT NULL,
+    tecnico VARCHAR(100) NOT NULL
+);
