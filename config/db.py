051c21bd95e43edd8be1561b0188a415b8459056
luizTavolaro <luiz.felipetaa@gmail.com>
2025-05-21 14:32:35 -0300

    feat: mysql connection

--- config/db.py
+++ config/db.py
@@ -1,6 +1,10 @@
-import sqlite3
+import mysql.connector
 
 def get_connection():
-    conn = sqlite3.connect("./config/test.db", timeout=5)
-    conn.row_factory = sqlite3.Row
-    return conn
+    return mysql.connector.connect(
+        host="nba-database.c0yjdr9hfgdy.us-east-1.rds.amazonaws.com",
+        port=3306,
+        user="admin",
+        password="admin@123!",
+        database='nba'
+    )
